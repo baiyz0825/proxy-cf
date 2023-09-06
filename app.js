a33d6755cd21293e6ddaf6f379804cc15de9e623
serverless-devs <serverless-devs@alibaba-inc.com>
2023-09-06 18:00:48 +0000

    "socket5"

--- app.js
+++ app.js
@@ -5,25 +5,34 @@
  * @Date: 2023-09-06 23:52:25
  * @LastEditTime: 2023-09-06 23:53:43
  */
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const socks = require('socks');
+const net = require('net');
 
-const app = express();
-const port = 9000;
-
-app.use('/', createProxyMiddleware({
-  target: 'https://api.themoviedb.org',
-  changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-    proxyReq.removeHeader('x-forwarded-for');
-    proxyReq.removeHeader('x-real-ip');
+// 代理服务器配置
+const proxyOptions = {
+  proxy: {
+    ipaddress: '0.0.0.0',  // 代理服务器监听的IP地址
+    port: 9000,           // 代理服务器监听的端口号
+    type: 5               // 使用Socket5协议
   },
-  onProxyRes: function (proxyRes, req, res) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
-}));
+  auths: [               // 配置用户名和密码验证
+    socks.auth.UserPassword('username', 'password')
+  ]
+};
 
-app.listen(port, () => {
-  console.log(`启动监听 http://localhost:${port}`);
+// 创建代理服务器
+const server = socks.createServer((info, accept, deny) => {
+  // 代理服务器收到连接请求时的处理逻辑
+  const socket = net.connect(info.port, info.host, () => {
+    accept(socket);
+  });
+
+  socket.on('error', (err) => {
+    deny();
+  });
+});
+
+// 启动代理服务器
+server.listen(proxyOptions.proxy.port, proxyOptions.proxy.ipaddress, () => {
+  console.log(`Socket5代理服务器已启动，监听在 ${proxyOptions.proxy.ipaddress}:${proxyOptions.proxy.port}`);
 });
